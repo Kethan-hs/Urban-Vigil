82b3268d6329c5878a2acce78e508bfaff8136bf
Kethan Shetty <kethanshetty05@gmail.com>
2025-11-09 18:17:43 +0530

    Frontend deployment error fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
-  build: { outDir: 'dist' }
+  build: {
+    outDir: '../public', // keep 'dist' if deploying separately on Vercel
+    emptyOutDir: true,
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html')
+    }
+  },
+  server: {
+    port: 5173
+  }
 })
